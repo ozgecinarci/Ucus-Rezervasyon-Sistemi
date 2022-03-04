a5d811c63269a1ae8d5f1ab7694bb6cb7e0835d7
ozgeozgec <89706915+ozgecinarci@users.noreply.github.com>
2022-03-04 15:55:49 +0300

    UcusRezervasyon

---
+++ Ucus_Rezervasyon_Sistemi/Ucus_Rezervasyon_Sistemi/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ucus_Rezervasyon_Sistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Rota: " + comboBox1.Text + " - " + comboBox2.Text+ "  Tarih: "+dateTimePicker1.Text+ "  Saat: "+ maskedTextBox1.Text);
+            listBox2.Items.Add(" Ad Soyad: " + textBox1.Text + "   TC Kimlik No: " + maskedTextBox2.Text + "  Telefon: " + maskedTextBox3.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label9.Text = comboBox2.Text;
+            comboBox2.Text = comboBox1.Text;
+            comboBox1.Text = label9.Text;
+        }
+    }
+}
